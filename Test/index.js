a0615d83565779b5d2a292a333eceeb75ed3977b
Shubham Wanne <shubhamwanne5@gmail.com>
2019-03-01 08:02:09 +0530

    Second commit

--- Test/index.js
+++ Test/index.js
@@ -1,16 +1,29 @@
 var Client = require('node-rest-client').Client;
-
+var fs = require('fs');
+var readline = require('readline');
 var client = new Client();
+var dataQueue=[];
 
-// set content-type header and data as json in args parameter
-var args = {
-    data: { test: "hello" },
-    headers: { "Content-Type": "application/json" }
-};
+var rd = readline.createInterface({
+  input: fs.createReadStream('sample2.txt')
+})
+
+rd.on('line', function(line) {
+  dataQueue.push(line);
+})
 
 function postData(){
+  if(dataQueue.length == 0)
+    return;
+  var fileData = dataQueue.shift();
+  var args = {
+      data: { value: fileData, timeStamp: new Date() },
+      headers: { "Content-Type": "application/json" }
+  };
+  console.log(`Sending ${fileData} to API`)
   client.post("http://127.0.0.1:5000/postJson", args, function (data, response) {
       console.log(data.toString('utf8'));
   });
 }
-setInterval(postData, 1000)
+
+setInterval(postData, 3000);
--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,6 @@ def index():    return "Hello, World!"# -*- coding: utf-8 -*-
 
 @app.route('/postJson', methods=['POST'])
 def post():
-    print(request.is_json)
     content = request.get_json()
     print(content)
     return 'JSON POSTED SUCCESSFULLY ...'
